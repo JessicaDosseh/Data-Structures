8b56e54deaa0c0132230e69da69a59bfb27ecca7
Jessica Dosseh <jessica.dosseh@yahoo.ca>
2020-09-24 16:33:21 -0400

    Refactor Queue with LinkedList, Test

--- queue/queue.py
+++ queue/queue.py
@@ -14,7 +14,13 @@ Stretch: What if you could only use instances of your Stack class to implement t
          What would that look like? How many Stacks would you need? Try it!
 """
 
+# from singly_linked_list import LinkedList
+# import sys
+# sys.path.append('../singly_linked_list/')
+from ..singly_linked_list.singly_linked_list import LinkedList
 
+'''
+Queue List
 class Queue:
     def __init__(self):
         self.size = 0
@@ -30,3 +36,22 @@ class Queue:
         if len(self.storage) == 0:
             return None
         return self.storage.pop(0)
+'''
+
+
+class Queue:
+    def __init__(self):
+        self.size = 0
+        self.storage = LinkedList()
+
+    def __len__(self):
+        return self.size
+
+    def enqueue(self, value):
+        self.size += 1
+        self.storage.add_to_tail(value)
+
+    def dequeue(self):
+        if self.size > 0:
+            self.size -= 1
+        return self.storage.remove_head()
