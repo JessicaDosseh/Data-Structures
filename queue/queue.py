0c4b242a0a4e590353a5811c339475a4cfbc13ff
Jessica Dosseh <jessica.dosseh@yahoo.ca>
2020-09-24 16:37:01 -0400

    Strech: Refactor Queue with Stack, Test

--- queue/queue.py
+++ queue/queue.py
@@ -14,10 +14,13 @@ Stretch: What if you could only use instances of your Stack class to implement t
          What would that look like? How many Stacks would you need? Try it!
 """
 
-# from singly_linked_list import LinkedList
 # import sys
 # sys.path.append('../singly_linked_list/')
+# sys.path.append('../stack/')
+# from singly_linked_list import LinkedList
+# from stack import Stack
 from ..singly_linked_list.singly_linked_list import LinkedList
+from ..stack.stack import Stack
 
 '''
 Queue List
@@ -38,7 +41,8 @@ class Queue:
         return self.storage.pop(0)
 '''
 
-
+'''
+Queue LinkedList
 class Queue:
     def __init__(self):
         self.size = 0
@@ -55,3 +59,32 @@ class Queue:
         if self.size > 0:
             self.size -= 1
         return self.storage.remove_head()
+'''
+
+
+class Queue:
+    def __init__(self):
+        self.size = 0
+        # 2 stacks required
+        self.push_stack = Stack()
+        self.pop_stack = Stack()
+
+    def __len__(self):
+        return self.size
+
+    def enqueue(self, value):
+        self.size += 1
+        self.push_stack.push(value)
+
+    def dequeue(self):
+        if self.size > 0:
+            self.size -= 1
+        # re-stack to reverse order
+        while self.push_stack.size > 0:
+            self.pop_stack.push(self.push_stack.pop())
+        # pop off one
+        value = self.pop_stack.pop()
+        # restack to original stack, original order
+        while self.pop_stack.size > 0:
+            self.push_stack.push(self.pop_stack.pop())
+        return value
